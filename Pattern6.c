317a64c1eac80af825fe9f4acd592ef4cf95e58c
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 13:27:07 +0530

    Create Pattern6.c
    				*			
    			*		*		
    		*		*		*	
    	*		*		*		*
    		*		*		*	
    			*		*		
    				*

---
+++ Pattern6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+     int i,j;
+     for(i=0;i<6;i++)
+       {
+               for(j=5;j>i;j--)
+               printf(“ “);
+               
+               for(i=0;j<I;j++)
+               printf(“* ”);
+
+                printf(“\n”);
+         }
+     
+      for(i=0;i<4;i++)
+        {
+                 for(j=0;j<I;j++)
+                 printf(“ ”);
+                 
+                  for(j=4;j>I;j--)
+                  printf(“* “);
+                      
+                  printf(“\n”);
+          }
+    }
+
