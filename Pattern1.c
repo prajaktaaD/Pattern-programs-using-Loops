4c4775cd1061945e4c105e363c4dc023e0377a7f
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 13:17:32 +0530

    Create Pattern1.c
    *			
    *	*		
    *	*	*	
    *	*	*	*

---
+++ Pattern1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    while(i<=4)
+     {
+          j=0;
+         while(j<i)
+           {
+                printf("*");
+                j++;
+            }
+            printf("\n");
+      }
+    }
