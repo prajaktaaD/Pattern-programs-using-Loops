8c633d002b65a7e97daafd970b9aadb376ec2b09
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 18:30:03 +0530

    Create Pattern9.c
    1			
    3	5		
    7	9	11	
    13	15	17	19

---
+++ Pattern9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+   int i,j,n=1;
+  for(i=1;i<6;i++)
+   {
+        for(j=1;j<I;j++)
+        {
+              printf(“%d ”,n);
+              n=n+2;
+           }
+           printf(“\n”);
+      }
+   } 
