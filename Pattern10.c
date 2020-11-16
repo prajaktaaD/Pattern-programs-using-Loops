e107f9c46f843396b2062cda712aae45296fa3af
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 18:31:29 +0530

    Create Pattern10.c
    				1				
    			2	1	2			
    		3	2	1	2	3		
    	4	3	2	1	2	3	4	
    5	4	3	2	1	2	3	4	5

---
+++ Pattern10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int I,j;
+    for(i=1;i<7;i++)
+    {
+           for(j=6;j>I;j--)
+           printf(“ “);
+
+           for(j=i-1;j>0;j--)
+           printf(“%d”,j);
+
+            for(j=2;j<I;j++)
+            printf(“%d”,j)’
+
+              Printf(“\n”);
+       }
+}
