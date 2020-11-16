9646275d01dffd02f087e0b19d4c8813a33ce91c
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 18:29:02 +0530

    Create Pattern8.c
    			A
    		B	A
    	C	B	A
    D	C	B	A

---
+++ Pattern8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+     int(i=0;i<4;i++)
+       {
+             for(j=4;j>i;j--)
+             printf(" ");
+              
+              for(j=i;j>=0;j--)
+              printf("%c",j+65);
+
+              printf("\n");
+        }
+    }   
+
