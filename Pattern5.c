e70d7151b1c2a02754345f654d11a27a219287d8
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 13:25:31 +0530

    Create Pattern5.c
    				*				
    			*		*			
    		*		*		*		
    	*		*		*		*	
    *		*		*		*		*

---
+++ Pattern5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+     int i,j;
+     for(i=0;i<6;i++)
+       {
+             for(j=5;j>i;j--)
+             printf(“ “);
+             
+              for(j=0;j<I;j++)
+               printf(“\n”);
+          }
+        }
+
